40018ca41cc801bf655ebf0e950b863f0c0489b0
Daniel <octoberrust@windowslive.com>
2022-05-04 07:27:10 +0200

    button

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,8 +9,8 @@ import * as L from 'leaflet';
 export class MapComponent implements AfterViewInit  {
 
   // @ts-ignore
-  private map;
-
+  private map:L.Map;
+  arr:any[]=[];
   private initMap(): void {
     this.map = L.map('map', {
       center: [ 39.8282, -98.5795 ],
@@ -24,6 +24,14 @@ export class MapComponent implements AfterViewInit  {
     });
 
     tiles.addTo(this.map);
+    this.map.addEventListener("click", (e:any) => {
+      debugger;
+      if (e.latlng) {
+        const { latlng } = e;
+        this.arr.push(latlng);
+      }
+      console.dir(e);
+    });
 
   }
 
@@ -31,6 +39,9 @@ export class MapComponent implements AfterViewInit  {
 
   ngAfterViewInit(): void {
     this.initMap();
+  }
+    create() {
+    debugger
   }
 
 }
